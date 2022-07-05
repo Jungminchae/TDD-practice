2289e14d748845253f260dc15b8c1ed0264e10db
Jungminchae <minchae3618@gmail.com>
2022-07-05 12:12:51 +0900

    Add: pytest setup & create user model test

---
+++ app/core/tests/test_models.py
@@ -0,0 +1,16 @@
+import pytest
+from django.contrib.auth import get_user_model
+
+
+pytestmark = pytest.mark.django_db
+
+
+def test_create_user_with_email_successful():
+    """
+    Test creating a new user with an email is successful
+    """
+    email = "test@example.com"
+    password = "Password123"
+    user = get_user_model().objects.create_user(email=email, password=password)
+    assert user.email == email
+    assert user.check_password(password)
